40c944aac204436b4da5120e63766c80a27a89de
Jared Long <jlong4223@gmail.com>
2023-04-20 16:08:33 -0500

    test: adds utils tests

---
+++ src/utils/tests/routesUtils.test.ts
@@ -0,0 +1,26 @@
+import { unstable_dev } from "wrangler";
+import type { UnstableDevWorker } from "wrangler";
+import { describe, expect, it, beforeAll, afterAll } from "vitest";
+import { setStatusCode, returnResponse } from "../routes";
+
+describe("Route utils", () => {
+  let worker: UnstableDevWorker;
+
+  beforeAll(async () => {
+    worker = await unstable_dev("src/index.ts", {
+      experimental: { disableExperimentalWarning: true },
+    });
+  });
+
+  afterAll(async () => {
+    await worker.stop();
+  });
+
+  it("should set the default status code to 200", () => {
+    expect(setStatusCode()).toMatchObject({ status: 200 });
+  });
+
+  it("should set the status code to 404", () => {
+    expect(setStatusCode(404)).toMatchObject({ status: 404 });
+  });
+});
