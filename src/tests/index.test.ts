703b81887d52403fe2a3f88d97a85cd863986c2f
Jared Long <jlong4223@gmail.com>
2023-04-20 16:12:41 -0500

    test: adds test for non-existent routes

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -22,4 +22,12 @@ describe("Worker", () => {
       expect(text).toMatchObject({ routeNote: "Welcome to the API" });
     }
   });
+
+  it("should return the 404 response", async () => {
+    const resp = await worker.fetch("/not-a-route");
+    if (resp) {
+      const text = await resp.json();
+      expect(text).toMatchObject({ routeNote: "No route found", status: 404 });
+    }
+  });
 });
