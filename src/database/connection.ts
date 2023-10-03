70e36512205b66de940432052418d3e5e3b77b43
Jared Long <jlong4223@gmail.com>
2023-10-03 09:46:02 -0500

    test: tries hardcoded secret for ci

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -2,7 +2,7 @@ import faunadb from "faunadb";
 
 export const faunaClient = new faunadb.Client({
   // @ts-ignore
-  secret: FAUNA_SECRET || "fnAFCCznjeAATXszZR6chXVs0v4-8o5c3yn8mKcb",
+  secret: FAUNA_SECRET ?? "fnAFCCznjeAATXszZR6chXVs0v4-8o5c3yn8mKcb",
 });
 
 export const {
