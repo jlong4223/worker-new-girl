3511126fef844588b54c0a3843f6fb1665a325ab
Jared Long <jlong4223@gmail.com>
2024-04-30 21:14:32 -0500

    refactor: uses faunadb utils for showInfo

--- src/database/documents/showInfo/showInfo.ts
+++ src/database/documents/showInfo/showInfo.ts
@@ -1,7 +1,7 @@
+import { getRawDataById } from "@gearsnbeans/faunadb-utils";
 import { Collections } from "../../collections";
-import { getSingleRefDataByID } from "../../queries";
 
 export const getShowInfo = async (id: string) => {
-  const showInfo = await getSingleRefDataByID(Collections.SHOW_INFO, id);
+  const showInfo = await getRawDataById(Collections.SHOW_INFO, id);
   return showInfo;
 };
