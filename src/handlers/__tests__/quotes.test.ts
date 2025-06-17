4f58bc3c7417ecbcde05e3ad1202a734bba5fae5
Jared Long <jlong4223@gmail.com>
2025-06-17 12:46:54 -0500

    test: skips unusable route tests

--- src/handlers/__tests__/quotes.test.ts
+++ src/handlers/__tests__/quotes.test.ts
@@ -1,51 +1,51 @@
-import { expect, beforeAll, afterAll, describe, it } from "vitest";
-import { UnstableDevWorker, unstable_dev } from "wrangler";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import { Unstable_DevWorker, unstable_dev } from "wrangler";
 import { nickMillerDBid } from "../../test-helpers/testData";
 
-describe("Quote Routes", () => {
-  let worker: UnstableDevWorker;
+describe.skip("Quote Routes", () => {
+	let worker: Unstable_DevWorker;
 
-  beforeAll(async () => {
-    const faunaSecret = process.env.FAUNA_SECRET as string;
-    const faunaSecretV10 = process.env.V10_FAUNA_SECRET as string;
+	beforeAll(async () => {
+		const faunaSecret = process.env.FAUNA_SECRET as string;
+		const faunaSecretV10 = process.env.V10_FAUNA_SECRET as string;
 
-    worker = await unstable_dev("src/index.ts", {
-      experimental: { disableExperimentalWarning: true },
-      vars: {
-        FAUNA_SECRET: faunaSecret,
-        V10_FAUNA_SECRET: faunaSecretV10,
-      },
-      config: "wrangler.toml",
-    });
-  });
+		worker = await unstable_dev("src/index.ts", {
+			experimental: { disableExperimentalWarning: true },
+			vars: {
+				FAUNA_SECRET: faunaSecret,
+				V10_FAUNA_SECRET: faunaSecretV10,
+			},
+			config: "wrangler.toml",
+		});
+	});
 
-  afterAll(async () => {
-    await worker.stop();
-  });
+	afterAll(async () => {
+		await worker.stop();
+	});
 
-  const quoteObjResponse = {
-    id: expect.any(String),
-    quote: expect.any(String),
-    characterId: expect.any(String),
-  };
+	const quoteObjResponse = {
+		id: expect.any(String),
+		quote: expect.any(String),
+		characterId: expect.any(String),
+	};
 
-  const characterQuotesResponse = {
-    quotes: expect.arrayContaining([expect.any(String)]),
-  };
+	const characterQuotesResponse = {
+		quotes: expect.arrayContaining([expect.any(String)]),
+	};
 
-  it("should get all quotes", async () => {
-    const resp = await worker.fetch("/quotes");
-    const resJSON = await resp.json();
+	it("should get all quotes", async () => {
+		const resp = await worker.fetch("/quotes");
+		const resJSON = await resp.json();
 
-    expect(resJSON).toEqual(
-      expect.arrayContaining([expect.objectContaining(quoteObjResponse)])
-    );
-  });
+		expect(resJSON).toEqual(
+			expect.arrayContaining([expect.objectContaining(quoteObjResponse)])
+		);
+	});
 
-  it("should get Nick Millers quotes by the character id", async () => {
-    const resp = await worker.fetch(`/quotes/${nickMillerDBid}`);
-    const resJSON = await resp.json();
+	it("should get Nick Millers quotes by the character id", async () => {
+		const resp = await worker.fetch(`/quotes/${nickMillerDBid}`);
+		const resJSON = await resp.json();
 
-    expect(resJSON).toEqual(characterQuotesResponse);
-  });
+		expect(resJSON).toEqual(characterQuotesResponse);
+	});
 });
